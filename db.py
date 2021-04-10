45d7b9508837ee23220cf12d1d6e11488edaf77d
Jussi M <jussmaki@cs.helsinki.fi>
2021-04-10 21:00:54 +0300

    fixed typo in configuration

--- db.py
+++ db.py
@@ -4,7 +4,7 @@ from os import getenv
 
 database_url = getenv("DATABASE_URL").replace("postgres://", "postgresql://") #to make heroku postgres database url compatible with SQLAlchemy 1.4.x :)
 app.config["SQLALCHEMY_DATABASE_URI"] = database_url
-app.config["SQLALCHEMY_TRACK_NOTIFICATIONS"] = False
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 if getenv("FLASK_ENV") == "development":
     app.config['SQLALCHEMY_ECHO'] = True
